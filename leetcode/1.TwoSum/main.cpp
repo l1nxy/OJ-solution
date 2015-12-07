f893dc6c1eec05b547fe29bd7863c379061969d1
linxy <l1nxy.zy@gmail.com>
2015-12-07 17:12:51 +0800

    the second solution for townums

--- leetcode/1.TwoSum/main.cpp
+++ leetcode/1.TwoSum/main.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <vector>
-
+#include <map>
 using namespace std;
 
 
 vector<int> TowSum(vector<int> &nums,int target)
 {
     vector<int> temp;
-    for (size_t i = 0; i < nums.size(); i++) {
-        for( size_t j=i+1;j<nums.size();j++)
-            if(nums[i]+nums[j]==target){
-                temp.push_back(i);
-                temp.push_back(j);
-                return temp;
-            }
-
+    map<int,int> m;
+    for (size_t i=0;i<nums.size();i++){
+        m[nums[i]]=i;
+    }
+    for(size_t i=0;i<nums.size();i++){
+        int t=target-nums[i];
+        if(m.find(t)!=m.end()&&m[t]!=m[nums[i]]){
+        temp.push_back(i+1);
+        temp.push_back(m[t]+1);
+        return temp;
+        }
     }
 }
 int main(int argc, char *argv[])
 
 {
-    vector<int> v={2,7,11,19};
-    vector<int> s=TowSum(v,9);
+    vector<int> v={3,2,4};
+    vector<int> s=TowSum(v,6);
     for(auto &a:s)
         cout<<a<<endl;
     return 0;
